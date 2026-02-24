84a67fe949a4a7fc41efc47de87a0ca200a8e9c4
Michael Burbank <burbank2@yahoo.com>
2026-02-24 12:38:39 -0700

    Create a separate directory to store different topics without provisioning each topic.

---
+++ Modules/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+    description = "The AWS region to deploy resources in."
+    type        = string
+    default     = "us-west-1"
+
+}
+variable "aws_profile" {
+    description = "The AWS CLI profile to use for authentication."
+    type        = string
+}
