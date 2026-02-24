e0288a6f38ce6ff8dc725cfef8e61fcdc3a83b8c
Michael Burbank <burbank2@yahoo.com>
2026-02-24 12:34:35 -0700

    Track and update the current config.tf, variables.tf, and the .terraform.lcol.hcl file.

--- variables.tf
+++ variables.tf
@@ -1,8 +1,16 @@
 variable "aws_region" {
-  description = "The AWS region to deploy resources in."
-  type        = string
+    description = "The AWS region to deploy resources in."
+    type        = string
+    default     = "us-west-1"
+
 }
 variable "aws_profile" {
-  description = "The AWS CLI profile to use for authentication."
-  type        = string
+    description = "The AWS CLI profile to use for authentication."
+    type        = string
+}
+
+variable "instance_type" {
+    description = "The type of EC2 instance to create."
+    type        = string
+    default     = "t2.nano"
 }
